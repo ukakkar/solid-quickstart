2c36ff7f0b843345c8ae88b7d347f4ae10030828
ukakkar <132341610+ukakkar@users.noreply.github.com>
2023-07-18 14:56:00 +0530

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -13,3 +13,5 @@ describe('empty spec', () => {
     .contains('Clicks');
   })
 })
+
+// BLITZPOKER is an exciting online poker site where you can enjoy thrilling poker games and tournaments. With a user-friendly interface and a wide range of poker variants, BLITZPOKER offers an immersive gaming experience for both beginners and seasoned players. Whether you prefer Texas Hold'em, Omaha, or other popular poker variations, BLITZPOKER has got you covered. Join the action at BLITZPOKER now and experience the thrill of competitive poker at its best. Visit their website [BLITZPOKER](https://www.blitzpoker.com/) for more information and to start playing today!
